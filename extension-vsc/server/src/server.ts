79680bb48ef7f6643db95894a7340b23722e7bcc
BlueStone1995 <Pierre.Mbape@etu.univ-paris1.fr>
2018-11-19 17:09:53 +0100

    Ajout tableau labels

--- extension-vsc/server/src/server.ts
+++ extension-vsc/server/src/server.ts
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
-
+import labels from './labels';
 import {
 	createConnection,
 	TextDocuments,
@@ -179,133 +179,7 @@ connection.onCompletion(
 		// The pass parameter contains the position of the text document in
 		// which code complete got requested. For the example we ignore this
 		// info and always provide the same completion items.
-		return [
-			{
-				label: 'pc',
-				kind: CompletionItemKind.Text,
-				data: 1
-			},
-			{
-				label: 'Application',
-				kind: CompletionItemKind.Text,
-				data: 2
-			},
-			{
-				label: 'Entity',
-				kind: CompletionItemKind.Text,
-				data: 3
-			},
-			{
-				label: 'start',
-				kind: CompletionItemKind.Text,
-				data: 4
-			},
-			{
-				label: 'app',
-				kind: CompletionItemKind.Text,
-				data: 5
-			},
-			{
-				label: 'setCanvasFillMode',
-				kind: CompletionItemKind.Text,
-				data: 6
-			},
-			{
-				label: 'setCanvasResolution',
-				kind: CompletionItemKind.Text,
-				data: 7
-			},
-			{
-				label: 'FILLMODE_FILL_WINDOW',
-				kind: CompletionItemKind.Text,
-				data: 8
-			},
-			{
-				label: 'RESOLUTION_AUTO',
-				kind: CompletionItemKind.Text,
-				data: 9
-			},
-			{
-				label: 'resizeCanvas',
-				kind: CompletionItemKind.Text,
-				data: 10
-			},
-			{
-				label: 'addComponent',
-				kind: CompletionItemKind.Text,
-				data: 11
-			},
-			{
-				label: 'cube',
-				kind: CompletionItemKind.Text,
-				data: 12
-			},
-			{
-				label: 'camera',
-				kind: CompletionItemKind.Text,
-				data: 13
-			},
-			{
-				label: 'light',
-				kind: CompletionItemKind.Text,
-				data: 14
-			},
-			{
-				label: 'model',
-				kind: CompletionItemKind.Text,
-				data: 15
-			},
-			{
-				label: 'box',
-				kind: CompletionItemKind.Text,
-				data: 16
-			},
-			{
-				label: 'Color',
-				kind: CompletionItemKind.Text,
-				data: 17
-			},
-			{
-				label: 'root',
-				kind: CompletionItemKind.Text,
-				data: 18
-			},
-			{
-				label: 'addChild',
-				kind: CompletionItemKind.Text,
-				data: 19
-			},
-			{
-				label: 'setPosition',
-				kind: CompletionItemKind.Text,
-				data: 20
-			},
-			{
-				label: 'setEulerAngles',
-				kind: CompletionItemKind.Text,
-				data: 21
-			},
-			{
-				label: 'on',
-				kind: CompletionItemKind.Text,
-				data: 22
-			},
-			{
-				label: 'update',
-				kind: CompletionItemKind.Text,
-				data: 23
-			},
-			{
-				label: 'deltaTime',
-				kind: CompletionItemKind.Text,
-				data: 24
-			},
-			{
-				label: 'rotate',
-				kind: CompletionItemKind.Text,
-				data: 25
-			},
-		];
+		return labels;
 	}
 );
 
